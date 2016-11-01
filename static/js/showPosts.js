dee6aa04443c49765d2681c6348cd7418d69bca6
jason-lei <jason.lei@columbia.edu>
2016-11-01 12:48:47 -0400

    Improved post formatting and changed meeting time to datetime format

--- static/js/showPosts.js
+++ static/js/showPosts.js
@@ -10,10 +10,10 @@ $(function() {
                     .attr('class', 'list-group-item')
                     .append(
                         $('<h2>').attr('class', 'list-group-item-heading'),
-                        $('<p id="desc">').attr('class', 'list-group-item-text'),
                         $('<p id="user">').attr('class', 'list-group-item-text'),
                         $('<p id="meetingTime">').attr('class', 'list-group-item-text'),
                         $('<p id="location">').attr('class', 'list-group-item-text'),
+                        $('<p id="desc">').attr('class', 'list-group-item-text'),
                         $('<p id="postTime">').attr('class', 'list-group-item-text')
                     ));
 
@@ -23,10 +23,10 @@ $(function() {
             $.each(postObj, function(index, value) {
                 post = $(div).clone();
                 $(post).find('h2').text(value.Headline);
-                $(post).find('#desc').text(value.Description);
                 $(post).find('#user').text(value.User);
                 $(post).find('#meetingTime').text(value.MeetingTime);
                 $(post).find('#location').text(value.Location);
+                $(post).find('#desc').text(value.Description);
                 $(post).find('#postTime').text(value.PostTime);
                 $('.posts').append(post);
             });
