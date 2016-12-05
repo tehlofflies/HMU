29440a305627366dd9f1453939dfb8b459ba20fa
emilysong <eks2138@columbia.edu>
2016-12-05 10:58:54 -0500

    demo 2 - addFollow adds row to tbl_followers

--- app.py
+++ app.py
@@ -181,6 +181,7 @@ def addFollow(followed_user_id):
 			print(type(_follower_user_id), file=sys.stderr)
 			print(type(_followed_user_id), file=sys.stderr)
 			cursor.callproc('sp_addFollow', (_follower_user_id, _followed_user_id))
+			conn.commit()
 			# cursor.callproc('sp_createUser',("jy","jy@columbia.edu","jy"))
 			cursor.close()
 			conn.close()
