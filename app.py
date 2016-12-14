39a32980ef6e21c7207aecf216508ce1c47f11b2
Jiayin Tang <jt2823@columbia.edu>
2016-12-14 18:33:53 -0500

    tbl_user doesn't exist???

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app.secret_key = 'why would I tell you my secret key?'
 app.config['MYSQL_DATABASE_USER'] = 'root'
 app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'HMU'
-app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 
 mysql.init_app(app)
 
