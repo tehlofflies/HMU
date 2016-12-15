e3d09aa87e03caf268072bea22075239908bb51a
Jiayin Tang <jt2823@columbia.edu>
2016-12-15 03:22:10 -0500

    took out password

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app.secret_key = 'why would I tell you my secret key?'
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'mysql'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'HMU'
 app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
 
